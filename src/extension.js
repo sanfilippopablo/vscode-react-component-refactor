6788e437fbd1f27376e3e6c6c5fe7812bf956b34
Pablo Sanfilippo <sanfilippopablo@gmail.com>
2018-11-02 01:53:35 -0300

    Works with javascriptreact language

--- src/extension.js
+++ src/extension.js
@@ -13,12 +13,6 @@ const getTransformer = type =>
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "react-component-refactor" is now active!'
-  );
-
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
@@ -36,13 +30,11 @@ function activate(context) {
     }
   );
 
-  console.log("Regustering code action provider");
   vscode.languages.registerCodeActionsProvider(
-    "javascript",
+    ["javascript", "javascriptreact"],
     {
       provideCodeActions(document, range) {
         const type = detectType(document.getText(range));
-        console.log({ type });
         if (type) {
           return [
             {
